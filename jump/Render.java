57e36bef4d12e6d35ae98e69787e4714a60eda75
JCronk0 <joshua.cronk@gmail.com>
2017-05-25 10:32:35 -0500

    Jumper game package and game classes
    by Josh Cronk

---
+++ jump/Render.java
@@ -0,0 +1,18 @@
+package jump;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+//JPanel utilization class
+public class Render extends JPanel{
+	
+	private static final long serialVersionUID = 45345L;
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		Game.jumper.repaint(g);
+	}
+
+}
